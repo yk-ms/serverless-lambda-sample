9b78f0dc1293835c1ffb59075a9e8a672cf1beb4
masayokoo <masahiro.yokoo@medicalnote.co.jp>
2018-09-13 19:09:49 +0900

    Get object from S3, and response json"

--- handler.js
+++ handler.js
@@ -1,14 +1,26 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+const aws = require('aws-sdk');
+aws.config.region = 'ap-northeast-1';
+const bucketName = 'serverless-lambda-sample';
+var s3 = new aws.S3();
+
+module.exports.hello = async (_event, _context) => {
+  const jsonObject = await getJson();
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: jsonObject.Body.toString(),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+async function getJson() {
+  try {
+    const params = {
+      Bucket: bucketName,
+      Key: 'sample.json'
+    };
+    return await s3.getObject(params).promise();
+  } catch(err) {
+    console.error(err);
+  }
+}
